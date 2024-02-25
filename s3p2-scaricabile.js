4c3b3439de86bce6bd826b1ada8bfb1799fd7fd2
Luca-Gerace <geraceluca91@gmail.com>
2024-02-25 10:08:13 +0100

    M2W3D4: consegna

---
+++ s3p2-scaricabile.js
@@ -0,0 +1,43 @@
+// Il tuo compito è creare un sito e-commerce per Smartphones. 
+// L'homepage che stai per creare sarà la vetrina dei prodotti disponibili, con qualche informazione per ciascun prodotto. 
+// Non sono richieste funzionalità di carrello/cassa.
+// - Completa gli esercizi dall'1 al 6 SOLAMENTE con HTML
+// - Completa gli altri esercizi con JS
+
+//ESERCIZI IN HTML
+
+// ESERCIZIO 1: Inserisci un tag h1 con il nome del tuo negozio
+// ESERCIZIO 2: Aggiungi una tabella con i 5 prodotti più in vista del tuo negozio
+// ESERCIZIO 3: Aggiungi per ogni prodotto un'immagine, un titolo, una descrizione e un prezzo
+// ESERCIZIO 4: Per ogni elemento della tabella aggiungi un link di Amazon al prodotto esistente
+// ESERCIZIO 5: Aggiungi un footer con il nome e l'indirizzo del tuo negozio
+// ESERCIZIO 6: Aggiungi un campo testuale in cui l'utente può lasciare un commento su un prodotto (al momento non serve inserire nessuna "vera" funzionalità di POST/salvataggio!)
+
+//ESERCIZI IN JS
+// ESERCIZIO 7: Scrivi una funzione per cambiare il contenuto del tag h1 in qualcos'altro
+// ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
+// ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel footer in un altro, fittizio
+// ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
+// ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
+// ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
+
+// Puntatori
+let btn = document.getElementById('button');
+let h1 = document.getElementsByTagName('h1');
+let link = document.getElementsByTagName('a');
+let img = document.getElementsByClassName('img');
+let address = document.getElementById('address');
+
+if (img.length > 0) {
+    console.log(img); // stampa il primo elemento con la classe "img" trovato
+  } else {
+    console.log("Nessun elemento con la classe 'img' trovato");
+  }
+
+btn.addEventListener('click', function() {
+    h1.innerHTML = "Nuovo h1";
+    document.body.style.backgroundColor = "#ddd";
+    address.innerHTML = "<strong>Via Luigi Verdi 13</strong>, Milano";
+    link.classList.add("new-class");
+    img.classList.toggle("hide");
+})
